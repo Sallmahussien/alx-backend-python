348710ea9542ed12746ea8bf4168a22cdbc19fd3
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-01-11 11:39:39 +0200

    Fix task 9

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -6,6 +6,6 @@ from typing import Iterable, Sequence, List, Tuple
 iterable = Iterable[Sequence]
 
 
-def element_length(lst: iterable) -> List[Tuple[iterable, int]]:
+def element_length(lst: iterable) -> List[Tuple[Sequence, int]]:
     """Returns list of tuples with iterable and length of iterable"""
     return [(i, len(i)) for i in lst]
