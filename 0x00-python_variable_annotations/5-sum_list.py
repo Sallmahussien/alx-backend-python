1f3783c044bf1c82d74764fced7be26dd2252289
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-01-11 11:08:31 +0200

    Add tasks from 0-8

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Defines sum_list function"""
+
+from typing import List
+
+
+def sum_list(sum_list: List[float]) -> int:
+    """Returns the sum of list of float numbers"""
+    return sum(sum_list)
